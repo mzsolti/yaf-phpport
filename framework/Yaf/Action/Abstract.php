555c69ac734cc9a027ff479842fab67cfdf5956d
WenJun <wenjun1055@gmail.com>
2013-08-08 14:03:52 +0800

    fix a bug
    fix a bug

--- framework/Yaf/Action/Abstract.php
+++ framework/Yaf/Action/Abstract.php
@@ -3,7 +3,7 @@
  * @todo check an example to see how does it work
  * Yaf Action Abstract
  */
-abstract class Yaf_Action_Abstract extends Yaf_Config_Abstract
+abstract class Yaf_Action_Abstract extends Yaf_Controller_Abstract
 {
     protected $_controller = null;
     public function execute()
